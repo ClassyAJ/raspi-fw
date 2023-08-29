ef0bde7e59fd53168e882ab4131e5f4c9eda12ac
ClassyAJ <aaron.beglau@gmail.com>
2023-08-29 18:14:03 +0200

    test

--- local_copy.py
+++ local_copy.py
@@ -4,10 +4,11 @@ import I2C_LCD_driver
 from time import *
 import subprocess
 
-def format_sd_card(device_path):
+def format_sd_card(device_path, src_folder):
     try:
-        #subprocess.run(["sudo", "umount", device_path], check=True)
+        subprocess.run(["sudo", "umount", device_path], check=True)
         subprocess.run(["sudo", "mkfs.vfat", "-F", "32", device_path], check=True)
+        subprocess.run(["sudo", "mount", device_path, src_folder], check=True)
         return True
     except subprocess.CalledProcessError:
         return False
@@ -40,8 +41,6 @@ def main():
                     mylcd.lcd_display_string("kopiert...", 2)
 
                     src_folder = '/media/pi/'
-                    src_files = os.listdir(src_folder)[0]
-                    src_path = os.path.join(src_folder, src_files)
 
                     existing_folders = [f for f in os.listdir(dest_folder) if os.path.isdir(os.path.join(dest_folder, f)) and f.startswith('SD_Karte_')]
                     if existing_folders:
@@ -60,15 +59,17 @@ def main():
                         mylcd.lcd_clear()
                         mylcd.lcd_display_string("Dateien okay,", 1)
                         mylcd.lcd_display_string("formatiere Karte...", 2)
+                        temp_folder = f"{src_folder}/TEMP"
 
                         try:
-                            if format_sd_card(sd_card_path):
+                            if format_sd_card(sd_card_path, temp_folder):
                                 print("SD-Karte erfolgreich formatiert.")
                                 mylcd.lcd_clear()
                                 mylcd.lcd_display_string("Karte formatiert!", 1)
                                 mylcd.lcd_display_string("Bereit", 2)
                                 process_success = True
-                                while os.path.exists(src_path):
+
+                                while os.path.exists(temp_folder):
                                     mylcd.lcd_clear()
                                     mylcd.lcd_display_string("Vorgang", 1)
                                     mylcd.lcd_display_string("erfolgreich", 2)
@@ -78,6 +79,7 @@ def main():
                                     mylcd.lcd_display_string("entnehmen", 2)
                                     sleep(4)
                                 mylcd.lcd_clear()
+
                             else:
                                 mylcd.lcd_clear()
                                 mylcd.lcd_display_string("Formatieren fehlgeschlagen!", 1)
