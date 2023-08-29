3ed73e6e7782496cfebda3ffa1b2f1003b2dde64
ClassyAJ <aaron.beglau@gmail.com>
2023-08-29 16:55:27 +0200

    initial commit

---
+++ local_copy.py
@@ -0,0 +1,100 @@
+import os
+import shutil
+import I2C_LCD_driver
+from time import *
+import subprocess
+
+def recursive_copy(src_folder, dest_path):
+    try:
+        for item in os.listdir(src_folder):
+            dest_path_temp = dest_path
+            s = os.path.join(src_folder, item)
+            if os.path.isdir(s):
+                recursive_copy(src_folder=s, dest_path=dest_path)
+            else:
+                dest_path_temp = os.path.join(dest_path, item)
+                if not os.path.exists(dest_path_temp) or os.stat(s).st_mtime - os.stat(dest_path_temp).st_mtime > 1:
+                    shutil.copy2(s, dest_path_temp)
+        return True, None
+    except Exception as err:
+        return False, err
+
+def main():
+    mylcd = I2C_LCD_driver.lcd()
+
+    # Durchsuche den Ordner "SD_Karten_Backup" nach vorhandenen Ordnern und ermittle den höchsten Counter
+    dest_folder = '/home/pi/temp'
+    existing_folders = [f for f in os.listdir(dest_folder) if os.path.isdir(os.path.join(dest_folder, f)) and f.startswith('SD_Karte_')]
+    if existing_folders:
+        counter = max([int(f.split('_')[-1]) for f in existing_folders])
+    else:
+        counter = 0
+
+    while True:
+        # Überprüfen, ob eine SD-Karte eingesteckt ist und Dateien enthält
+        try:
+            if os.listdir('/media/pi'):
+                print("SD-Karte wird kopiert...")
+                mylcd.lcd_clear()
+                mylcd.lcd_display_string("SD-Karte wird", 1)
+                mylcd.lcd_display_string("kopiert...", 2)
+
+                # Erstellen eines Ordners für die SD-Karte auf dem Desktop
+                src_folder = '/media/pi/'
+                dest_folder = '/home/pi/temp/'
+
+                # Erhöhe den Zähler um 1
+                counter += 1
+                folder_name = f"SD_Karte_{counter}"
+                dest_path = os.path.join(dest_folder, folder_name)
+                os.makedirs(dest_path, exist_ok=True)
+
+                process_success = False
+
+                # Kopieren aller Dateien von der SD-Karte in den Ordner für diese SD-Karte
+                copy_success = recursive_copy(src_folder, dest_path)
+
+                if copy_success[0]:
+                    print("Alle Dateien erfolgreich kopiert, formatiere SD-Karte...")
+                    mylcd.lcd_clear()
+                    mylcd.lcd_display_string("Dateien okay,", 1)
+                    mylcd.lcd_display_string("formatiere Karte...", 2)
+
+                    try:
+                        # Hier wird die SD-Karte formatiert (Achtung: Datenverlust!)
+                        subprocess.run(['sudo', 'rm -r', f"{src_folder}*"])  # Passe den Gerätenamen entsprechend an
+
+                        print("SD-Karte erfolgreich formatiert.")
+                        mylcd.lcd_clear()
+                        mylcd.lcd_display_string("Karte formatiert!", 1)
+                        mylcd.lcd_display_string("Bereit", 2)
+                        process_success = True
+                    except Exception as err:
+                        print(err)
+                        mylcd.lcd_clear()
+                        mylcd.lcd_display_string("Formatieren fehlgeschlagen!", 1)
+                        mylcd.lcd_display_string("Bereit", 2)
+
+                else:
+                    print("Kopieren nicht erfolgreich, bitte Karte erneut einführen...")
+                    print(copy_success[1])
+                    mylcd.lcd_clear()
+                    mylcd.lcd_display_string("Kopieren fehlgeschlagen,", 1)
+                    mylcd.lcd_display_string("Karte erneut einführen...", 2)
+
+            else:
+                print("Bereit")
+                mylcd.lcd_display_string("Bereit", 1)
+                sleep(1)
+                mylcd.lcd_clear()
+        except Exception as err:
+            print("Etwas ist schiefgelaufen")
+
+        finally:
+            if not process_success:
+                # Wiederherstellen der ursprünglichen Ordnung (falls benötigt)
+                if os.path.exists(dest_path):
+                    shutil.rmtree(dest_path)
+
+if __name__ == "__main__":
+    main()
---
+++ upload_webdav.py
@@ -0,0 +1,73 @@
+from enum import Enum
+import os, errno
+import sys
+import shutil
+import subprocess
+
+# Definiere die Pfade
+class Constants(Enum):
+    LOCKFILE = '/tmp/webdav_sync.lock'
+    TEMP_FOLDER = '/home/pi/temp'
+    WEBDAV_MOUNT_POINT = '/mnt/webdav'
+    WEBDAV_SHARE_URL = 'https://webdav.example.com/share'
+    WEBDAV_USERNAME = 'your_username'
+    WEBDAV_PASSWORD = 'your_password'
+    WEBDAV_AUTH = f'username={WEBDAV_USERNAME},password={WEBDAV_PASSWORD}'
+
+try:
+    # Überprüfe, ob das Skript bereits ausgeführt wird
+    if os.path.exists(Constants.LOCKFILE.value):
+        print("Das Skript wird bereits ausgeführt.")
+        sys.exit(1)
+
+    # Erstelle eine Lock-Datei, um zu verhindern, dass das Skript mehrmals gleichzeitig ausgeführt wird
+    with open(Constants.LOCKFILE.value, 'w') as lock_file:
+        lock_file.write(str(os.getpid()))
+
+    # Mounte den WebDAV-Share
+    mount_success = False
+    try:
+        subprocess.run(['sudo', 'mount', '-t', 'cifs', '-o', Constants.WEBDAV_AUTH.value, Constants.WEBDAV_SHARE_URL.value, Constants.WEBDAV_MOUNT_POINT.value])
+        # FIXME nur weil der Code ausgeführt wurde und status 1 meldet heißt das nicht, dass das Mounting auch funktioniert hat :D
+        mount_success = True
+    except Exception as e:
+        print("Fehler beim Mounten des WebDAV-Shares:", e)
+
+    if mount_success:
+        # Kopiere Dateien
+        for item in os.listdir(Constants.TEMP_FOLDER.value):
+            src_path = os.path.join(Constants.TEMP_FOLDER.value, item)
+            dest_path = os.path.join(Constants.WEBDAV_MOUNT_POINT.value, item)
+            if os.path.isfile(src_path) and not os.path.exists(dest_path):
+                try:
+                    shutil.copy2(src_path, dest_path)
+                    print(f"Datei {item} kopiert.")
+                except Exception as e:
+                    # TODO Dateien sammeln und in der Exception (Zeile 60) in einen backup Folder kopieren
+                    # beim nächsten Durchlauf des Skripts diese Dateien dann ebenfalls kopieren
+                    print(f"Fehler beim Kopieren von {item}:", e)
+
+        # Bereinige den temporären Ordner
+        for item in os.listdir(Constants.TEMP_FOLDER.value):
+            item_path = os.path.join(Constants.TEMP_FOLDER.value, item)
+            if os.path.isfile(item_path):
+                os.remove(item_path)
+            elif os.path.isdir(item_path):
+                shutil.rmtree(item_path)
+
+        # Trenne den WebDAV-Share
+        subprocess.run(['sudo', 'umount', Constants.WEBDAV_MOUNT_POINT.value])
+
+except Exception as err:
+    print(f"Es gab einem Fehler beim Ausführen des Programms: {err}")
+
+# Dieser Codeblock wird immer ausgeführt, ob das Programm vorher crasht oder nicht
+finally:
+    try:
+        # löscht die lock Datei
+        os.remove(Constants.LOCKFILE.value)
+    except OSError as err:
+        # Datei/ordner existiert nicht (enoent)
+        if e.errno != errno.ENOENT:
+            # raise bei einem anderen OSError
+            raise
